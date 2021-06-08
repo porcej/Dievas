ffa7b8dec573c2289f7e7b913b8a66ac245c6cb6
Joe Porcelli <porcej@gmail.com>
2021-06-08 04:16:34 -0400

    Added configuraiton injection

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -16,6 +16,7 @@ namespace backend
 {
     public class Startup
     {
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,13 +27,13 @@ namespace backend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "backend", Version = "v1" });
             });
             services.AddSignalR();
+            services.AddSingleton<IConfiguration>(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
