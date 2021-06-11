9dc592c341df6654b82f2553694a1a076266cd42
Joe Porcelli <porcej@gmail.com>
2021-06-10 20:52:17 -0400

    Added dashboard view

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -19,6 +19,11 @@ const routes = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  {
+    path: "/dash",
+    name: "Dashboard",
+    component: () => import("../views/Dash.vue"),
+  },
 ];
 
 const router = new VueRouter({
---
+++ frontend/src/views/Dash.vue
@@ -0,0 +1,70 @@
+<template>
+  <b-container fluid class="h-100 station-dashboard">
+    <b-row class="h-100">
+      <b-col cols="3" :style="wxBorderStyle"></b-col>
+      <b-col :style="wxContentStyle">
+        <!-- 900000ms = 15 minutes -->
+        <vue-telestaff
+          station="Station 209"
+          :updateInterval="15 * 60 * 1000"
+          title="Today"
+        />
+      </b-col>
+      <b-col :style="wxContentStyle">
+        <!-- 900000ms = 15 minutes -->
+        <vue-telestaff
+          station="Station 209"
+          date="20210614"
+          :updateInterval="15 * 60 * 1000"
+        />
+      </b-col>
+    </b-row>
+    <b-row class="footer fixed-bottom" :style="wxBorderStyle">
+      <b-col cols="4" class="align-middle">
+        <vue-clock :showBackground="false" />
+      </b-col>
+      <b-col cols="8">
+        <vue-weather
+          weatherUrl="https://localhost:5001/api/WeatherForecast/"
+          :updateInterval="900000"
+          :hideHeader="true"
+        />
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import VueWeather from "@/components/VueWeatherWidget";
+import VueClock from "@/components/VueDigitalClock";
+import VueTelestaff from "@/components/VueTelestaff";
+
+export default {
+  components: {
+    VueWeather,
+    VueClock,
+    VueTelestaff,
+  },
+  data() {
+    return {
+      wxBorderStyle: {
+        backgroundColor: "#222222", // Vampire Christmas Theme
+        // backgroundColor: "#050A30", // Blue Theme
+        // backgroundColor: "#323031", // Coffee Theme
+      },
+      wxContentStyle: {
+        backgroundColor: "inherit", // Vampire Christmas Theme
+        // backgroundColor: "#000C66", // Blue Theme
+        // backgroundColor: "#3d3b3c", // Coffee Theme
+      },
+    };
+  },
+};
+</script>
+<style>
+.station-dashboard {
+  font-family: "Frutiger CE 55 Roman";
+  /*font-family: "Frutiger CE 65 Bold";*/
+  color: #7ec8e3 !important;
+}
+</style>
