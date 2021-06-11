da1e4928722e74c9f1318b02fa7550ba18b51dd4
Joe Porcelli <porcej@gmail.com>
2021-06-10 20:53:49 -0400

    Added bootstrap-vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <div id="app">
-    <div id="nav">
+  <div id="app" class="bg-dark h-100">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/wx">Weather</router-link>
+    </div> -->
     <router-view />
   </div>
 </template>
 
 <style>
+body,
+html {
+  /*background-color: #121212;*/
+  height: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
