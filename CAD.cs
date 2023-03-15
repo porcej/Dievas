23cb33254ba8d991ac746135db3de21825a54bef
Joe Porcelli <porcej@gmail.com>
2023-03-15 16:15:35 -0400

    Remvoed unused references to Dashboard.Models

--- CAD.cs
+++ CAD.cs
@@ -4,7 +4,6 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Backend.Models;
 using AFD.Dashboard.Models;
 
 namespace Dievas {
--- Models/NWSForecast.cs
+++ Models/NWSForecast.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Generic;
-using Backend.Models;
+using Dievas.Models;
 
 namespace Dievas.Models {
 
