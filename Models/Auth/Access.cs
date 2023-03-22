6abdab3994cd5576f59adc7fab77af38667765a9
Joe Porcelli <porcej@gmail.com>
2023-03-22 08:27:48 -0400

    Added auth models

---
+++ Models/Auth/Access.cs
@@ -0,0 +1,12 @@
+using Dievas.Models.Auth;
+
+namespace Dievas.Models.Auth
+{
+    // We are not taking data from data base so we get data from constant
+    public class Access
+    {
+        public static List<UserModel> Users = new() {
+            new UserModel(){ Username="testadmin", Roles= new List<string> {"admin"}}
+        };
+    }
+}
---
+++ Models/Auth/UserModel.cs
@@ -0,0 +1,8 @@
+namespace Dievas.Models.Auth
+{
+    public class UserModel
+    {
+        public string Username { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
