eb363ae2c05d7acfc9152d6974533bbdfa911b06
Joe Porcelli <porcej@gmail.com>
2023-09-19 15:21:19 -0400

    Added ProbabilityOfPrecipitation, DewPoint, and RelativeHumidity to forecast data fiedls

--- Models/NWSPeriodForecast.cs
+++ Models/NWSPeriodForecast.cs
@@ -2,18 +2,42 @@ using System;
 
 namespace Dievas.Models {
 
+
+    /// <summary>
+    ///     Class <c>NWSPeriodForecast</c> holds weather forecast data for one period
+    ///     as defined by api.weather.gov
+    /// </summary>
     public class NWSPeriodForecast {
         public int Number { get; set; }
         public string Name { get; set; }
-        public DateTime startTime { get; set; }
-        public DateTime endTime { get; set; }
-        public bool isDayTime { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public bool IsDayTime { get; set; }
         public int Temperature { get; set; }
-        public string temperatureUnit { get; set; }
-        public string windSpeed { get; set; }
-        public string windDirection { get; set; }
+        public string TemperatureUnit { get; set; }
+        public string WindSpeed { get; set; }
+        public string WindDirection { get; set; }
         public string Icon { get; set; }
-        public string shortForecast { get; set; }
-        public string detailedForecast { get; set; }
+        public string ShortForecast { get; set; }
+        public string DetailedForecast { get; set; }
+        public NWSProbabilityOfPrecipitation ProbabilityOfPrecipitation { get; set; }
+        public NWSDewPoint DewPoint { get; set; }
+        public NWSRelativeHumidity RelativeHumidity { get; set; }
+
+    }
+
+    public class NWSProbabilityOfPrecipitation {
+        public string UnitCode { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class NWSDewPoint {
+        public string UnitCode { get; set; }
+        public string Value { get; set; }    
+    }
+
+    public class NWSRelativeHumidity{
+        public string UnitCode { get; set; }
+        public string Value { get; set; }  
     }
 }
