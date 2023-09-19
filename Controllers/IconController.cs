4b4254f571a3c8e019a1a1e81d4f481b00afab2b
Joe Porcelli <porcej@gmail.com>
2023-09-19 14:29:58 -0400

    Added icon controller to serve up hot icons

---
+++ Controllers/IconController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dievas.Hubs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Dievas.Controllers {
+
+
+    /// <summary>
+    ///     Controller Class <c>IconController</c> serves up hot icons
+    ///     
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IconController : ControllerBase {
+
+        private readonly IConfiguration _config;
+
+        private readonly ILogger<IconController> _logger;
+
+        public DashboardController(IConfiguration configuration,
+                                   ILogger<IconController> logger) {
+
+            _config = configuration;
+            _logger = logger;
+        }
+
+        // Readonly web API for incidents information
+        [HttpGet("marker/{callType}")]
+        public IActionResult GetIncidents(string callType) {
+            FileStream stream = File.Open(@"")
+            return _cad.GetIncidents();
+        }
+    }
+}
